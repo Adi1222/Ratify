a65ff5e28ce77da21df49e1ca64290c2b8a48022
Aditya Chavan <adityachavan677@gmail.com>
2020-10-29 20:45:44 +0530

    added 3 more urls

--- src/reviews/urls.py
+++ src/reviews/urls.py
@@ -3,7 +3,16 @@ from . import views
 
 urlpatterns = [
     path('login/', views.login_request, name='login'),
+
     path('signup/', views.signup, name='signup'),
+
     path('home/', views.homepage, name='homepage'),
+
+    path('review/<int:review_id>/', views.review_detail, name='review_detail'),
+
+    path('review/username>/', views.user_review_list, name='user_review_list'),
+
+    path('categories/', views.categories, name='categories'),
+
     path('logout/', views.logout_request, name='logout_request'),
 ]

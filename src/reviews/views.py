1f4011f5835dbc09dd95ad4099dd74162ff75c50
Aditya Chavan <adityachavan677@gmail.com>
2020-11-28 20:49:08 +0530

    topics logic added

--- src/reviews/views.py
+++ src/reviews/views.py
@@ -74,10 +74,9 @@ def homepage(request):
     top_products = []
     allproducts = Product.objects.all()
 
-    '''if allproducts != None:
-        for product in Product.objects.all():
-            if product.average_rating >= 3 and product.total_rating > 10:
-                top_products.push(product)'''
+    for p in Product.objects.all():
+        if p.average_rating() >= 3:
+            top_products.append(p)
 
     return render(request, 'reviews/homepage.html', {'latest_review_list': latest_review_list, 'top_products': top_products})
 
